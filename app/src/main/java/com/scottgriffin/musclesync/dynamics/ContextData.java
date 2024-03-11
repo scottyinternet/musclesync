2d69f4efbd719acc0dbe62f15d0c8b08c430c480
Scott Griffin <griffin.scott88@gmail.com>
2024-03-11 01:07:49 -0500

    everything is working quite well. 2 functionalSystemTests run dynamically, updating the flow, the context, and adding context to the test

--- app/src/main/java/com/scottgriffin/musclesync/dynamics/ContextData.java
+++ app/src/main/java/com/scottgriffin/musclesync/dynamics/ContextData.java
@@ -2,48 +2,94 @@ package com.scottgriffin.musclesync.dynamics;
 
 import com.scottgriffin.musclesync.model.SystemTest;
 import com.scottgriffin.musclesync.model.Test;
+import com.scottgriffin.musclesync.model.enums.ISA_RESULT;
 import com.scottgriffin.musclesync.model.enums.PregnancyStatus;
 import com.scottgriffin.musclesync.model.enums.RESULT_ENUM;
 import com.scottgriffin.musclesync.model.enums.ROTATION_RESULT;
-
-import java.util.Map;
 
 public class ContextData {
 	PregnancyStatus pregnancyStatus;
 	RESULT_ENUM isaResult;
 	int excessiveFlags = 0;
+	RESULT_ENUM hromInferedISAResult;
+
+	// FLAG CLASS
+	// TRACKS IF IT HAS BEEN ALERTED
+	// HOLDS A MESSAGE
+	// CAN TAKE IN A RESULT.. LIKE check hypermobility? confirm, deny, ask again later, ignore
+	// sub types - flag counter (as in hyper mobility)
+	// inferred type difference... ISA
 
 	public ContextData updateContextData(SystemTest systemTestResult) {
 		System.out.println("UPDATE CONTEXT DATA () ");
 		String systemTestId = systemTestResult.getSystemTestId();
-		if (systemTestId.equals("ISA::ST")) {
-			Test isaTest = systemTestResult.getTestMap().get("ISA01");
-			isaResult = isaTest.getResultOptions().getResult(isaTest.getResultValue());
-			System.out.println("UPDATE ISA RESULT TO " + isaResult);
-		} else if (systemTestId.equals("HROM::ST")){
-			// check excessive
-			for(Map.Entry<String, Test> entry : systemTestResult.getTestMap().entrySet()) {
-				Test test = entry.getValue();
-				RESULT_ENUM result = test.getResultOptions().getResult(test.getResultValue());
-				if (result == ROTATION_RESULT.EXCESSIVE) {
-					excessiveFlags++;
-				}
-			}
+
+		switch (systemTestId) {
+			case "ISA::ST":
+				isaHandler(systemTestResult);
+				break;
+			case "HROM::ST":
+				hromHandler(systemTestResult);
+				break;
+			case "SROM::ST":
+				break;
+			case "PO::ST":
+				break;
+			case "STK::ST":
+				break;
+			default:
+				System.out.println("DEFAULT");
 		}
 		return this;
 	}
 
+	private void isaHandler(SystemTest systemTestResult) {
+		Test isaTest = systemTestResult.getTestMap().get("ISA01");
+		isaResult = isaTest.getResultOptions().getResult(isaTest.getResultValue());
+		System.out.println("UPDATE ISA RESULT TO " + isaResult);
+	}
+
+	private void hromHandler(SystemTest systemTestResult) {
+		checkExcessive(systemTestResult);
+		inferISA(systemTestResult);
+	}
+
+	private void inferISA(SystemTest systemTest) {
+		Test externalTest = systemTest.getTestMap().get("HROM01");
+		Test internalTest = systemTest.getTestMap().get("HROM02");
+		RESULT_ENUM externalResult = externalTest.getResultOptions().getResult(externalTest.getResultValue());
+		RESULT_ENUM internalResult = internalTest.getResultOptions().getResult(internalTest.getResultValue());
+		if (externalResult == ROTATION_RESULT.LIMITED && internalResult != ROTATION_RESULT.LIMITED) {
+			hromInferedISAResult = ISA_RESULT.WIDE;
+		} else if (internalResult == ROTATION_RESULT.LIMITED && (externalResult != ROTATION_RESULT.LIMITED)) {
+			hromInferedISAResult = ISA_RESULT.NARROW;
+		} else {
+			hromInferedISAResult = ISA_RESULT.NEUTRAL;
+		}
+	}
+
+	private void checkExcessive(SystemTest systemTest) {
+		for(Test test : systemTest.getTestMap().values()) {
+			RESULT_ENUM result = test.getResultOptions().getResult(test.getResultValue());
+			if (result == ROTATION_RESULT.EXCESSIVE) {
+				excessiveFlags++;
+			}
+		}
+	}
+
 	public void checkFlags() {
 		if (excessiveFlags > 1) {
-			System.out.println("!! ALERT !! - Check HyperMobility");
+			System.out.println("!! FLAG !! - Check HyperMobility");
+		}
+
+		if (hromInferedISAResult != null && isaResult != null && hromInferedISAResult != isaResult) {
+			System.out.println("!! FLAG !! - Check RibFlair: Isa Result: " + isaResult + " != HROM inferred result: " + hromInferedISAResult);
 		}
 	}
 
 	public ContextData updateContextData(PregnancyStatus pregnancyStatus) {
 		return this;
 	}
-
-
 
 	public PregnancyStatus getPregnancyStatus() {
 		return pregnancyStatus;
